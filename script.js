808c3ee89dc72d4b31ad6124f7f3814cb6a5a887
jyash28 <jyash548@gmail.com>
2021-04-20 22:35:14 +0530

    update

---
+++ script.js
@@ -0,0 +1,33 @@
+const quizDB = [
+    {
+        question: "Q4 : What is the fullform of html",
+        a: "hello to my land",
+        b: "hey text markup language",
+        c: "hyper text markup language",
+        d: "hyper textmarkup language",
+        ans:"ans4"
+    },
+    {
+        question: "Q1 : What is the fullform of html",
+        a: "hello to my land",
+        b: "hey text markup language",
+        c: "hyper text markup language",
+        d: "hyper textmarkup language",
+        ans:"ans4"
+    },
+
+];
+
+const question = document.querySelector('.question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+const submit =  document.querySelector('#submit');
+
+const loadQuestion = () => {
+    
+}
+
+loadQuestion();
+
